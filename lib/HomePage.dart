f87241fddfb149ebd422ab6ff7e8fc545089764d
rakibul-hasan212 <egnrakibul212@gmail.com>
2026-04-02 00:41:48 +0600

    Icon Colors error fix

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'customWidgets/MyTextFormFeild.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   @override
@@ -10,24 +10,42 @@ class HomePage extends StatelessWidget{
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Welcome to the Home page",
+          Text(
+            "Welcome!",
             style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 40,
-              color: Colors.black
-          ),),
-          SizedBox(height: 20,),
+                fontWeight: FontWeight.bold, fontSize: 40, color: Colors.black),
+          ),
+          SizedBox(
+            height: 20,
+          ),
           Padding(
-            padding: const EdgeInsets.only(left: 30,right: 30),
-            child: MyTextFormFeild(
-              labelName: "Email",
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: Column(
+              children: [
+                MyTextFormFeild(
+                  labelName: "Email",
+                  hintMessage: "Enter your email",
+                  preIconName: Icons.email_outlined,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                MyTextFormFeild(
+                  labelName: "Password",
+                  hintMessage: "Type your password",
+                  preIconName: Icons.lock_outlined,
+                  suffixIconName: Icons.visibility_outlined,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
             ),
           ),
         ],
       ),
-
     );
   }
-
 }
--- lib/customWidgets/MyTextFormFeild.dart
+++ lib/customWidgets/MyTextFormFeild.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MyTextFormFeild extends StatelessWidget{
+class MyTextFormFeild extends StatelessWidget {
   //label attribute
   final String labelName;
   final Color? labelNameColors;
   final double? labelNameFontSize;
+
   //hint attribute
   final String? hintMessage;
   final double? hintMessageFontSize;
   final Color? hintMessageColors;
-
 
   final IconData? preIconName;
   final Color? preIconColor;
@@ -19,47 +19,46 @@ class MyTextFormFeild extends StatelessWidget{
 
   final double? intialRadius;
 
-  MyTextFormFeild({
-    super.key,
-    required this.labelName,
-    this.labelNameColors = Colors.grey,
-    this.labelNameFontSize = 20,
-    this.hintMessage = " ",
-    this.hintMessageColors = Colors.grey,
-    this.hintMessageFontSize = 20,
-    this.preIconName,
-    this.preIconColor = Colors.grey,
-    this.suffixIconName,
-    this.suffixIconColor = Colors.grey,
-    this.intialRadius = 16
-  });
+  MyTextFormFeild(
+      {super.key,
+      required this.labelName,
+      this.labelNameColors = Colors.grey,
+      this.labelNameFontSize = 20,
+      this.hintMessage = " ",
+      this.hintMessageColors = Colors.grey,
+      this.hintMessageFontSize = 20,
+      this.preIconName,
+      this.preIconColor = Colors.grey,
+      this.suffixIconName,
+      this.suffixIconColor = Colors.grey,
+      this.intialRadius = 16});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: TextEditingController(),
+      //controller: TextEditingController(), since it's a statelesswidget that's why it comment
       //keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
-        label: Text(labelName),
-        labelStyle: TextStyle(
-          fontSize: labelNameFontSize,
-          fontWeight: FontWeight.bold,
-          color: labelNameColors
-        ),
-        hintText: hintMessage,
-        hintStyle: TextStyle(
-            fontSize: hintMessageFontSize,
-            fontWeight: FontWeight.bold,
-            color: hintMessageColors
-        ),
-        prefixIcon: Icon(preIconName),
-        suffixIcon: Icon(suffixIconName),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(intialRadius!)
-        )
-          
-      ),
-
+          label: Text(labelName),
+          labelStyle: TextStyle(
+              fontSize: labelNameFontSize,
+              fontWeight: FontWeight.bold,
+              color: labelNameColors),
+          hintText: hintMessage,
+          hintStyle: TextStyle(
+              fontSize: hintMessageFontSize,
+              fontWeight: FontWeight.bold,
+              color: hintMessageColors),
+          prefixIcon: Icon(
+            preIconName,
+            color: preIconColor,
+          ),
+          suffixIcon: Icon(
+            suffixIconName,
+            color: suffixIconColor,
+          ),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(intialRadius!))),
     );
   }
 }
